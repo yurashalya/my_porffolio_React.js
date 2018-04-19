c6a484c7f731027ead8112f52a6a7bc32591bcc8
Yura <stallker9696@gmail.com>
2018-04-19 22:36:25 +0300

    Head section content filled  / icons/title/links

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,45 @@ class Landing extends Component {
     return(
       <div style={{width: '100%', margin: 'auto'}}>
       	<Grid className="landing-grid">
-      		<Cell col={12}></Cell>
+      		<Cell col={12}>
+      			<img 
+      				src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+      				alt="avatar"
+      				className="avatar-img"
+      			/>
+
+      			<div className="banner-text">
+      				<h1>Front-end Web Developer</h1>
+
+      			<hr/>
+
+      			<p>HTML/CSS | Bootstrap | JavaScript | SmartGrid | Git | React</p>
+
+      			<div className="social-links">
+
+						{/* Linked In */}
+					<a href="https://google.com" rel="noopener noreferrer" target="_blank">
+						<i className="fa fa-linkedin-square" aria-hidden="true"/>
+					</a>
+
+					{/* Instagram */}
+					<a href="https://google.com" rel="noopener noreferrer" target="_blank">
+						<i className="fa fa-instagram" aria-hidden="true"/>
+					</a>
+
+					{/* Github */}
+					<a href="https://google.com" rel="noopener noreferrer" target="_blank">
+						<i className="fa fa-github-square" aria-hidden="true"/>
+					</a>
+
+					{/* YouTube */}
+					<a href="https://google.com" rel="noopener noreferrer" target="_blank">
+						<i className="fa fa-youtube-square" aria-hidden="true"/>
+					</a>
+
+      			 </div>
+      			</div>
+      		</Cell>
       	</Grid>
       </div>
     )
